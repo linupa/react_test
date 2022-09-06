a469e28f901e0731749dd1140822e69fcf2f8307
Kwan Suk Kim <linupa@gmail.com>
2022-09-05 20:59:35 -0700

    Updates

---
+++ precache-manifest.ff72c9769a6c04c794160553add007dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react_test/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "0c2b6735491a6911455b",
+    "url": "/react_test/static/js/runtime~main.0c2b6735.js"
+  },
+  {
+    "revision": "e94cf1609aa5b174859f",
+    "url": "/react_test/static/js/main.e94cf160.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/react_test/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react_test/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "e94cf1609aa5b174859f",
+    "url": "/react_test/static/css/main.cbb8f4df.chunk.css"
+  },
+  {
+    "revision": "221eaf6acf78cc14b4f53a95f0777f51",
+    "url": "/react_test/index.html"
+  }
+];
